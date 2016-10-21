1e9a62a27401f43212b010adb138706e41dae7c9
Pedro Saldanha <papajorgioster@gmail.com>
2016-10-21 21:11:45 +0100

    Basic Procedural Generation with Cubes at different height

---
+++ Assets/Scripts/MapGenerator/MapDisplay.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDisplay : MonoBehaviour {
+
+	public Renderer textureRender;
+    public GameObject baseMap;
+
+    public void DrawTexture(Texture2D texture) {
+		textureRender.sharedMaterial.mainTexture = texture;
+		textureRender.transform.localScale = new Vector3 (texture.width, 1, texture.height);
+	}
+
+    public void removeMap()
+    {
+        Debug.Log("before  " + baseMap.transform.childCount);
+        foreach (Transform child in baseMap.transform)
+        {
+            GameObject.DestroyImmediate(child.gameObject);
+        }
+        Debug.Log("After  " + baseMap.transform.childCount);
+        if(baseMap.transform.childCount > 0)
+        {
+            Debug.Log("removeAgain");
+            removeMap();
+        }
+    }
+
+    public void DrawCubes(TerrainTexture[] textures, float[,] noiseMap, int mapWidth, int mapHeight, float heightDescrepancy, float cubeScale)
+    {
+        //Removes the old map
+        removeMap();
+
+        //Debug.Log("mapHeight   " + mapHeight + "mapWidth   " + mapWidth);
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < textures.Length; i++)
+                {
+                    if (currentHeight <= textures[i].height)
+                    {
+                        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        cube.transform.parent = baseMap.transform;
+                        cube.transform.localScale = new Vector3(cubeScale, cubeScale, cubeScale);
+                        cube.transform.position = new Vector3(x - (mapWidth / 2) + baseMap.transform.position.x, (int)(currentHeight * heightDescrepancy), y - (mapHeight / 2) + baseMap.transform.position.z) * cubeScale;
+                        cube.GetComponent<Renderer>().material = textures[i].material;
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        
+    }
+
+}
---
+++ Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+	public enum DrawMode {NoiseMap, ColourMap, TextureTerrain };
+	public DrawMode drawMode;
+
+	public int mapWidth;
+	public int mapHeight;
+	public float noiseScale;
+
+	public int octaves;
+	[Range(0,1)]
+	public float persistance;
+
+    [Range(0, 10)]
+    public float heightDescrepancy;
+    public float cubeScale;
+
+
+    public float lacunarity;
+
+	public int seed;
+	public Vector2 offset;
+
+	public bool autoUpdate;
+
+	public TerrainType[] regions;
+    public TerrainTexture[] textures;
+
+    void Start()
+    {
+        GenerateMap(Random.Range(0, 1000));
+    }
+
+    public void GenerateMap(int seed)
+    {
+        this.seed = seed;
+        GenerateMap();
+    }
+
+    public void GenerateMap() {
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+
+        float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+		Color[] colourMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions [i].height) {
+						colourMap [y * mapWidth + x] = regions [i].colour;
+						break;
+					}
+				}
+			}
+		}
+
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture (TextureGenerator.TextureFromHeightMap(noiseMap));
+		} else if (drawMode == DrawMode.ColourMap) {
+			display.DrawTexture (TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
+        else if(drawMode == DrawMode.TextureTerrain)
+        {
+            display.DrawCubes(textures, noiseMap, mapWidth, mapHeight, heightDescrepancy, cubeScale);
+        }
+	}
+
+	void OnValidate() {
+		if (mapWidth < 1) {
+			mapWidth = 1;
+		}
+		if (mapHeight < 1) {
+			mapHeight = 1;
+		}
+		if (lacunarity < 1) {
+			lacunarity = 1;
+		}
+		if (octaves < 0) {
+			octaves = 0;
+		}
+	}
+}
+
+[System.Serializable]
+public struct TerrainType {
+	public string name;
+	public float height;
+    public Color colour;
+}
+
+[System.Serializable]
+public struct TerrainTexture
+{
+    public string name;
+    public float height;
+    public Material material;
+}
